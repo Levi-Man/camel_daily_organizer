7250211d15fa41aa841e36467db2092939f6c5a9
Indra Levi Manahan <indra.levi.manahan@gmail.com>
2023-11-28 19:23:14 -0500

    Added button to generate new random quote

--- assets/script.js
+++ assets/script.js
@@ -1,20 +1,23 @@
 let quoteContainer = document.getElementById("quote-text");
 let authorContainer = document.getElementById("author-name")
-let x = Math.floor(Math.random() * 16);
+let newRandom = document.getElementById('new-random');
+newRandom.addEventListener('click', randomQuote);
 
-fetch("https://type.fit/api/quotes")
-  .then(function(response) {
-    return response.json();
-  })
-  .then(function(data) {
-    console.log(data);
-  let ranQuote=data[x].text;
-  let ranAuthor=data[x].author;
-    let myArray = ranAuthor.split(",");
+randomQuote();
 
+function randomQuote() {
+  fetch("https://type.fit/api/quotes")
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+      let x = Math.floor(Math.random() * data.length);
+      let ranQuote = data[x].text;
+      let ranAuthor = data[x].author;
+      let myArray = ranAuthor.split(",");
+      quoteContainer.textContent = ranQuote;
+      authorContainer.textContent = myArray[0];
+    });
+}
 
-
-  quoteContainer.textContent=ranQuote;
-  authorContainer.textContent=myArray[0];
-
-  });
